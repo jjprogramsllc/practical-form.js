7182109d694e3aaf43126dc2c2ed4333bc6d2910
Jospeh Livecchi <joewashear007@gmail.com>
2014-10-06 13:36:25 -0400

    Many small fixes * Updated the readme with better documentation and layout * Updted the confirm popup scope values * Variuous small fixes to others

--- src/components/ui-bootstrap/Confirm Dialog/confirm.js
+++ src/components/ui-bootstrap/Confirm Dialog/confirm.js
@@ -2,17 +2,20 @@ module.directive("pfConfirm", ['$modal', function($modal){
   return {
     restrict: 'A',
     scope: {
-	    title: "@",
+	    pfTitle: "@",
+      pfMessage: "@",
       pfConfirm: "&",
-      item: "="
     },
     link : function ($scope, $element, $attrs) {
       $element.bind('click', function () {
 
-        var message = $attrs.pfMessage || "Are you sure ?";
-        var modalHtml = '<div class="modal-header"><h1>'+$scope.title+'</h1></div>';
+        var message = $scope.pfMessage || "Are you sure ?";
+        var title = $scope.pfTitle || "Warning!";
+        var modalHtml = '<div class="modal-header"><h1>'+title+'</h1></div>';
         modalHtml += '<div class="modal-body">' + message + '</div>';
-        modalHtml += '<div class="modal-footer"><button class="btn btn-primary" ng-click="Ok()">OK</button><button class="btn btn-warning" ng-click="Cancel()">Cancel</button></div>';
+        modalHtml += '<div class="modal-footer">';
+        modalHtml += '<button class="btn btn-primary" ng-click="Ok()">OK</button>';
+        modalHtml += '<button class="btn btn-warning" ng-click="Cancel()">Cancel</button></div>';
 
         var modalInstance = $modal.open({
           template: modalHtml,
@@ -21,9 +24,8 @@ module.directive("pfConfirm", ['$modal', function($modal){
             $scope.Cancel = function() { $modalInstance.dismiss('cancel'); };
           }],
         });
-
         modalInstance.result.then(function () {
-            $scope.pfConfirm({ item: $scope.item });
+            $scope.pfConfirm();
         }, function () {
             //Modal dismissed
         });
