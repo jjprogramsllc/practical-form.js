b41d66948d535202270ab047a1d29f7b95c4a847
Joseph Livecchi <joewashear007@gmail.com>
2015-07-28 11:13:29 -0400

    Gave Some love to confirm dialog
    The confirm dialog got some updates, it can now use a custom modal
    options object

--- dist/practical-forms.js
+++ dist/practical-forms.js
@@ -1,4 +1,4 @@
-/*! practical-forms v0.4.1 | (c) 2015, JJ Programs, LLC | Distributed under the MIT License */
+/*! practical-forms v0.5.0 | (c) 2015, JJ Programs, LLC | Distributed under the MIT License */
 angular.module('jjp.practical-forms.templates', []).run(['$templateCache', function($templateCache) {
   $templateCache.put("/jjp/pf/checkbox.html",
     "<div ng-class=\"{'has-error':!ngModel && ngRequired, 'has-success':ngModel&& ngRequired }\"><div class=checkbox tabindex=-1><label for={{::id}}><input id={{::id}} ng-model=ngModel ng-required={{ngRequired}} type=\"checkbox\">{{title}}&nbsp; <span class=pf-required ng-show=\"required || ngRequired\">*</span></label></div></div>");
@@ -814,32 +814,32 @@ angular.module('jjp.practical-forms.templates', []).run(['$templateCache', funct
         pfTitle: "@",
         pfMessage: "@",
         pfConfirm: "&",
+        modalOptions: "="
       },
       link: function($scope, $element, $attrs) {
         $element.bind('click', function() {
-          var message = $scope.pfMessage || "Are you sure ?";
-          var title = $scope.pfTitle || "Warning!";
-          var modalHtml = '<div class="modal-header"><h1>' + title + '</h1></div>';
+          var message = $scope.pfMessage || "Please confirm this action!";
+          var title = $scope.pfTitle || "Are you sure?";
+
+          var modalHtml = '<div class="modal-header"><h2>' + title + '</h2></div>';
           modalHtml += '<div class="modal-body">' + message + '</div>';
           modalHtml += '<div class="modal-footer">';
           modalHtml += '<button class="btn btn-primary" ng-click="Ok()">OK</button>';
           modalHtml += '<button class="btn btn-warning" ng-click="Cancel()">Cancel</button></div>';
 
-          var modalInstance = $modal.open({
-            template: modalHtml,
-            controller: ['$scope', '$modalInstance', function($scope, $modalInstance) {
-              $scope.Ok = function() {
-                $modalInstance.close();
-              };
-              $scope.Cancel = function() {
-                $modalInstance.dismiss('cancel');
-              };
-            }],
-          });
-          modalInstance.result.then(function() {
+          $scope.modalOptions = $scope.modalOptions || {};
+          $scope.modalOptions.template = $scope.modalOptions.template || modalHtml;
+          $scope.modalOptions.controller = $scope.modalOptions.controller || ['$scope', '$modalInstance', function($scope, $modalInstance) {
+            $scope.Ok = function() {
+              $modalInstance.close();
+            };
+            $scope.Cancel = function() {
+              $modalInstance.dismiss('cancel');
+            };
+          }];
+
+          $modal.open($scope.modalOptions).result.then(function() {
             $scope.pfConfirm();
-          }, function() {
-            //Modal dismissed
           });
         });
       }
