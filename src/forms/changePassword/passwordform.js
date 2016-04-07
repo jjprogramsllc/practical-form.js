9cdd59603a46f35bb224cbc2499c60a1a2e674bd
Joseph Livecchi <joewashear007@gmail.com>
2016-04-07 16:45:27 -0400

    Added disabled options to the forms
    * Added the disbaled options to the form to disabled the entire form at the whole time

--- src/forms/changePassword/passwordform.js
+++ src/forms/changePassword/passwordform.js
@@ -14,6 +14,7 @@
     return {
       scope: {
         ngModel: '=',
+        ngDisabled: '=?',
         ngSubmit: '&',
         settings: '=?',
       },
--- src/forms/register/registerform.js
+++ src/forms/register/registerform.js
@@ -16,6 +16,7 @@
     return {
       scope: {
         ngModel: '=',
+        ngDisabled: '=?',
         ngSubmit: '&',
         settings: '=?'
       },
